3201c390df7d4de7348df158bb16a5aee22f0c54
han <102780666+seungeunhan@users.noreply.github.com>
2024-05-01 16:22:54 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+		char s = sc.next().charAt(0); //입력받은 문자 저장
+	
+		System.out.println((int)s); //문자를 int형으로 변경
+	}//--main
+
+}//--class
